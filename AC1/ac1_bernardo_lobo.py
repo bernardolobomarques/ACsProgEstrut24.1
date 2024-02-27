852fad7cf05db3ac3ce545af3edbb500a757b483
bernardolobomarques <161499154+bernardolobomarques@users.noreply.github.com>
2024-02-27 20:09:11 -0300

    ADD AC1

---
+++ AC1/ac1_bernardo_lobo.py
@@ -0,0 +1,32 @@
+"""
+
+PROGRAMAÇÃO ESTRUTURADA 2024.1
+AC 1
+BERNARDO LOBO MARQUES - MATRICULA: 202401709433
+27.02.24
+
+"""
+
+
+def exercicio1():
+    a = int(input("Qual o valor do \"a\" da funcao? "))
+    b = int(input("Qual o valor do \"b\" da funcao? "))
+    c = int(input("Qual o valor do \"c\" da funcao? "))
+
+    resultado1 = (-b + ((b**2 - 4*a*c)**(1/2)))/2*a
+    resultado2 = (-b - ((b**2 - 4*a*c)**(1/2)))/2*a
+    print("A primeira raiz da equacao e", resultado1, "\n", "E a segunda raiz da equacao e", resultado2)
+    return(resultado1, resultado2)
+
+def exercicio2():
+    ano = int(input("Qual o ano que gostaria de saber se e ou nao bissexto? "))
+    controle = ano%4
+    print(controle == 0)
+
+
+def main():
+    #  exercicio1()
+    #  exercicio2()
+
+#Deixei comentado para caso queira testa-los um por um, portanto o erro na main()
+main()
