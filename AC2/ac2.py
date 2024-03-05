7750b13c275157450a2aad260ef51fd10a126b5c
bernardolobomarques <161499154+bernardolobomarques@users.noreply.github.com>
2024-03-05 14:51:44 -0300

    Add files via upload

--- AC2/ac2.py
+++ AC2/ac2.py
@@ -12,7 +12,7 @@ def exercicio1_1(a,b,c):
     resultado1 = (-b + ((b**2 - 4*a*c)**(1/2)))/(2*a)
     resultado2 = (-b - ((b**2 - 4*a*c)**(1/2)))/(2*a)
     print("A primeira raiz da equacao e", resultado1, "\n", "E a segunda raiz da equacao e", resultado2)
-    return(resultado1, resultado2)
+    return resultado1, resultado2
 
 def exercicio1_2(ano):
     print((ano%4) == 0 and ((ano % 400 == 0) or not(ano%100 == 0)))
