b1498a7c9af8af7893b78889ee669ce8a09508c2
bernardolobomarques <161499154+bernardolobomarques@users.noreply.github.com>
2024-03-09 13:53:35 -0300

    Update ac2.py

--- AC2/ac2.py
+++ AC2/ac2.py
@@ -8,15 +8,19 @@ BERNARDO LOBO MARQUES - MATRICULA: 202401709433
 """
 
 
-def exercicio1_1(a,b,c):
+def eq_seg_grau(a, b, c):
     resultado1 = (-b + ((b**2 - 4*a*c)**(1/2)))/(2*a)
     resultado2 = (-b - ((b**2 - 4*a*c)**(1/2)))/(2*a)
     return resultado1, resultado2
 
-def exercicio1_2(ano):
-    retrurn((ano%4) == 0 and ((ano % 400 == 0) or not(ano%100 == 0)))
+def bissexto(ano):
+    return((ano%4) == 0 and ((ano % 400 == 0) or not(ano%100 == 0)))
 
 def calcula_salario(valor_hora, num_horas, irpf = 0.275):
     salario_bruto = valor_hora*num_horas
     salario_liquido = salario_bruto - (salario_bruto*irpf)
     return salario_liquido
+
+print(eq_seg_grau(1,1,1))
+print(bissexto(1900))
+print(calcula_salario(10, 10))
