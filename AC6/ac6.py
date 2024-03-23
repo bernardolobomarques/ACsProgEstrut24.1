73ce3ac7f07b9195cec86d89b9bff6a1824d4a48
Seu NomeBernardo <bernardolobomarques@gmail.com>
2024-03-23 16:19:59 -0300

    ac6 att

--- AC6/ac6.py
+++ AC6/ac6.py
@@ -8,29 +8,44 @@ def ex_1001():
     print("X =", x)
 
 def ex_1010():
-    a = int(input())
-    b = int(input())
-    c = float(input())
+    dados1 = input()
+    dados2 = input()
 
-    d = int(input())
-    e = int(input())
-    f = float(input())
-
-    valor = e*f + b*c
-    print(f"VALOR A PAGAR: R$ {valor:.2f}")
+    dados1 = dados1.split(" ")
+    dados2 = dados2.split(" ")
+    preco = int(dados1[1]) * float(dados1[2]) + int(dados2[1]) * float(dados2[2])
+    print(f"VALOR A PAGAR: R$ {preco:.2f}")
 
 # ex_1010()
     
 def ex_1013():
-    x = int(input())
-    y = int(input())
-    z = int(input())
+    dados = input()
 
-    if x>y and x > z:
-        print(x, "eh o maior")
-    elif y>z and y>x:
-        print(y, "eh o maior")
-    elif z>y and z>x:
-        print(z, "eh o maior")
+    dados = dados.split(" ")
+    dados[0] = int(dados[0])
+    dados[1] = int(dados[1])
+    dados[2] = int(dados[2])
 
-ex_1013()
+    if dados[0]>dados[1] and dados[0] > dados[2]:
+        print(dados[0], "eh o maior")
+    elif dados[1]>dados[0] and dados[1]>dados[2]:
+        print(dados[1], "eh o maior")
+    elif dados[2]>dados[0] and dados[2]>dados[1]:
+        print(dados[2], "eh o maior")
+
+# ex_1013()
+        
+def ex_1017():
+    a = input()
+    a = a.split(" ")
+    a[0] = float(a[0])
+    a[1] = float(a[1])
+    b = input()
+    b = b.split(" ")
+    b[0] = float(b[0])
+    b[1] = float(b[1])
+
+    distancia = (((b[0] - a[0])**2)+((b[1]-a[1])**2))**(1/2)
+    print(f"{distancia:.4f}")
+
+ex_1017()
